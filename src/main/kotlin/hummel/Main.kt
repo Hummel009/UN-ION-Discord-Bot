d401aa8fc5c42ed62e4bf44795d72bbd684434c9
Hummel009 <hummel.turbamentum@gmail.com>
2023-10-16 22:15:49 +0300

    json, data saving

--- src/main/kotlin/hummel/Main.kt
+++ src/main/kotlin/hummel/Main.kt
@@ -1,8 +1,11 @@
 package hummel
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import org.javacord.api.DiscordApiBuilder
 import org.javacord.api.entity.intent.Intent
-import java.io.File
+import java.io.*
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Path
@@ -10,28 +13,29 @@ import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
 import java.util.*
 
+
 val rand: Random = Random()
 
 fun main() {
 	val token = File("token.txt").readText(StandardCharsets.UTF_8)
-
 	val api = DiscordApiBuilder().setToken(token).addIntents(*Intent.values()).login().join()
-
-	val chance = HashMap<Long, Int>()
-	val lastEditTime = HashMap<Long, Long>()
 
 	api.addMessageCreateListener { event ->
 		val serverID = event.server.get().id
-		val folderPath = Paths.get("$serverID")
+		val serverName = event.server.get().name
+		val chance = "10"
+
+		val folderPath = Paths.get(serverID.toString())
 		if (!Files.exists(folderPath)) {
 			Files.createDirectories(folderPath)
 		}
+
 		val path = folderPath.resolve("messages.bin")
 		if (!Files.exists(path)) {
 			Files.createFile(path)
 		}
 
-		lastEditTime[serverID] = Files.getLastModifiedTime(path).toMillis()
+		val lastUpdateDb = Files.getLastModifiedTime(path).toMillis()
 
 		if (event.messageAuthor.isBotOwner) {
 			if (event.messageContent == "!clear database") {
@@ -39,11 +43,19 @@ fun main() {
 				event.channel.sendMessage("Database cleared.")
 			}
 			if (event.messageContent.startsWith("!chance")) {
+				val data = mutableMapOf(
+					"serverID" to serverID.toString(),
+					"serverName" to serverName,
+					"chance" to chance,
+					"lastUpdateDb" to lastUpdateDb.toString()
+				)
+
 				val parts = event.messageContent.split(" ")
 				if (parts.size >= 2) {
 					try {
-						chance[serverID] = parts[1].toInt()
-						event.channel.sendMessage("Chance changed to ${chance[serverID]}.")
+						data["chance"] = parts[1].toInt().toString()
+						saveDataToJson(data, "$serverID/data.json")
+						event.channel.sendMessage("Chance changed to ${data["chance"]}.")
 					} catch (e: NumberFormatException) {
 						event.channel.sendMessage("Invalid integer format after !chance.")
 					}
@@ -61,10 +73,25 @@ fun main() {
 
 			val editTime = Files.getLastModifiedTime(path).toMillis()
 
-			if (rand.nextInt(chance[serverID] ?: 10) == 0 && editTime > (lastEditTime[serverID] ?: 0L)) {
+			var data = readDataFromJson("$serverID/data.json")
+
+			if (data == null) {
+				data = mutableMapOf(
+					"serverID" to serverID.toString(),
+					"serverName" to serverName,
+					"chance" to chance,
+					"lastUpdateDb" to lastUpdateDb.toString()
+				)
+				saveDataToJson(data, "$serverID/data.json")
+			}
+
+			if (rand.nextInt(data["chance"]?.toInt() ?: 10) == 0 && editTime > (data["lastUpdateDb"]?.toLong() ?: 0L)) {
 				val randomLine = path.getRandomLine()
-				event.channel.sendMessage(randomLine)
-				lastEditTime[event.server.get().id] = editTime
+				randomLine?.let {
+					event.channel.sendMessage(it)
+					data["lastUpdateDb"] = editTime.toString()
+					saveDataToJson(data, "$serverID/data.json")
+				}
 			}
 		}
 	}
@@ -73,17 +100,42 @@ fun main() {
 }
 
 fun String.isMessageForbidden(): Boolean {
-	return this.length < 2 || startsWith("!") || contains("@") || contains("http") || contains("\r") || contains("\n")
+	return this.length < 2 || startsWith("!") || startsWith("?") || contains("@") || contains("http") || contains("\r") || contains("\n")
 }
 
-fun Path.getRandomLine(): String {
+fun Path.getRandomLine(): String? {
 	val lines = Files.readAllLines(this)
-	return if (lines.isNotEmpty()) {
+	if (lines.isNotEmpty()) {
 		val randomLine = lines[rand.nextInt(lines.size)]
-		val unicodeCodes = randomLine.split(" ").map { it.toInt() }
-		val unicodeChars = unicodeCodes.map { it.toChar() }.toCharArray()
-		String(unicodeChars)
+		if (!randomLine.isMessageForbidden()) {
+			val unicodeCodes = randomLine.split(" ").map { it.toInt() }
+			val unicodeChars = unicodeCodes.map { it.toChar() }.toCharArray()
+			return String(unicodeChars)
+		}
+		return null
 	} else {
-		""
+		return null
 	}
+}
+
+fun saveDataToJson(data: Map<String, String>, filePath: String) {
+	try {
+		FileWriter(filePath).use {
+			val gson = GsonBuilder().setPrettyPrinting().create()
+			gson.toJson(data, it)
+		}
+	} catch (ignored: IOException) {
+	}
+}
+
+fun readDataFromJson(filePath: String): MutableMap<String, String>? {
+	try {
+		BufferedReader(FileReader(filePath)).use { reader ->
+			val gson = Gson()
+			val mapType = object : TypeToken<Map<String, String>>() {}.type
+			return gson.fromJson(reader, mapType)
+		}
+	} catch (ignored: IOException) {
+	}
+	return null
 }
