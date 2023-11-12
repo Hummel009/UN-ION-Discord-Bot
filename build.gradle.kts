c8e5c6c1252dbc3132fae80e43f7c3cd96a07f87
Hummel009 <hummel.turbamentum@gmail.com>
2023-11-12 18:31:24 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,9 +30,6 @@ dependencies {
 }
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
 	sourceCompatibility = JavaVersion.VERSION_17
 	targetCompatibility = JavaVersion.VERSION_17
 }
