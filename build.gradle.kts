d039ac5c8235f0b63058c2b993c3f55c491bc381
Hummel009 <hummel.turbamentum@gmail.com>
2024-01-14 17:33:33 +0300

    Revert "kotlin 1.9.22"
    This reverts commit 1cd02380739b391d28c68b067d1402bf15758fbc.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	id("com.android.application") version "8.1.4" apply false
-	id("org.jetbrains.kotlin.jvm") version "1.9.22" apply false
-	id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+	id("org.jetbrains.kotlin.jvm") version "1.9.21" apply false
+	id("org.jetbrains.kotlin.android") version "1.9.21" apply false
 	id("org.jetbrains.compose") version "1.5.11" apply false
 }
