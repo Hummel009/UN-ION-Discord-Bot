b7330f77d3f77a58426146fc8629d19185adef54
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-01-16 13:48:22 +0300

    to compat

--- appAndroid/build.gradle.kts
+++ appAndroid/build.gradle.kts
@@ -47,9 +47,9 @@ android {
 dependencies {
 	implementation("androidx.core:core-ktx:1.12.0")
 	implementation("androidx.appcompat:appcompat:1.6.1")
-	implementation("com.google.android.material:material:1.10.0")
+	implementation("com.google.android.material:material:1.11.0")
 
-	implementation("androidx.activity:activity-compose:1.8.1")
+	implementation("androidx.activity:activity-compose:1.8.2")
 	implementation(platform("androidx.compose:compose-bom:2022.10.00"))
 
 	implementation("androidx.compose.ui:ui")
--- appWindows/src/main/kotlin/hummel/utils/Extensions.kt
+++ appWindows/src/main/kotlin/hummel/utils/Extensions.kt
@@ -3,13 +3,12 @@ package hummel.utils
 import hummel.bean.ServerData
 import org.javacord.api.entity.message.embed.EmbedBuilder
 import org.javacord.api.interaction.SlashCommandInteraction
-import java.awt.Color
 
 fun EmbedBuilder.error(sc: SlashCommandInteraction, serverData: ServerData, desc: String): EmbedBuilder =
-	setAuthor(sc.user).setTitle(Lang.MSG_ERROR[serverData]).setColor(Color.RED).setDescription(desc)
+	setAuthor(sc.user).setTitle(Lang.MSG_ERROR[serverData]).setDescription(desc)
 
 fun EmbedBuilder.access(sc: SlashCommandInteraction, serverData: ServerData, desc: String): EmbedBuilder =
-	setAuthor(sc.user).setTitle(Lang.MSG_ACCESS[serverData]).setColor(Color.ORANGE).setDescription(desc)
+	setAuthor(sc.user).setTitle(Lang.MSG_ACCESS[serverData]).setDescription(desc)
 
 fun EmbedBuilder.success(sc: SlashCommandInteraction, serverData: ServerData, desc: String): EmbedBuilder =
-	setAuthor(sc.user).setTitle(Lang.MSG_SUCCESS[serverData]).setColor(Color.GREEN).setDescription(desc)
+	setAuthor(sc.user).setTitle(Lang.MSG_SUCCESS[serverData]).setDescription(desc)
