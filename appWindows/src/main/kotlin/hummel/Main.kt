d2aa3b2be2adad45460da17ca02f5705b8e63776
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-01-16 17:55:21 +0300

    style issues

--- appWindows/src/main/kotlin/hummel/Main.kt
+++ appWindows/src/main/kotlin/hummel/Main.kt
@@ -27,14 +27,14 @@ fun main() {
 			resizable = false
 		) {
 			MaterialTheme {
-				composableOnCreate()
+				ComposableOnCreate()
 			}
 		}
 	}
 }
 
 @Composable
-private fun composableOnCreate() {
+private fun ComposableOnCreate() {
 	var token: String by remember { mutableStateOf("TOKEN") }
 	var ownerId: String by remember { mutableStateOf("1186780521624244278") }
 
