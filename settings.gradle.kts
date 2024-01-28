b3ac95407d873b110697ff3d6b008492df6b979e
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-01-28 20:09:25 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,10 @@ dependencyResolutionManagement {
 	}
 }
 
+dependencyResolutionManagement {
+	repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+}
+
 plugins {
 	id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
