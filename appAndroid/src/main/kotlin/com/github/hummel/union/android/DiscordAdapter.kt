44e1996ec609a65e7d8a51bbf0d806ee86187dcd
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-08 22:10:04 +0300

    test

--- appAndroid/src/main/kotlin/com/github/hummel/union/android/DiscordAdapter.kt
+++ appAndroid/src/main/kotlin/com/github/hummel/union/android/DiscordAdapter.kt
@@ -10,6 +10,7 @@ import android.os.IBinder
 import android.os.PowerManager
 import android.os.PowerManager.WakeLock
 import androidx.core.app.NotificationCompat
+import com.github.hummel.union.R
 import com.github.hummel.union.controller.DiscordController
 import com.github.hummel.union.controller.impl.DiscordControllerImpl
 
@@ -27,14 +28,18 @@ class DiscordAdapter : Service() {
 	@SuppressLint("WakelockTimeout")
 	override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 		wakeLock.acquire()
-		val channelId = "HundroidId1"
-		val channelName = "HundroidChannel1"
-		val notification = NotificationCompat.Builder(this, channelId).run {
+		val channelId = "Hummel009id1"
+		val channelName = "Hummel009channel1"
+		val notificationBuilder = NotificationCompat.Builder(this, channelId).run {
+			setContentTitle("Foreground Service")
+			setContentText("Your service is running")
+			setSmallIcon(R.drawable.ic_launcher_background)
 			setPriority(NotificationCompat.PRIORITY_MAX)
-		}.build()
-		val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+		}
+		val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
 		val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 		notificationManager.createNotificationChannel(channel)
+		val notification = notificationBuilder.build()
 		startForeground(1, notification)
 		controller.onStartCommand()
 		return START_STICKY
