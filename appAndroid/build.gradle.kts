04d08d56f49ac033ea120ef6ad63cd2589024b52
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-10 18:42:21 +0300

    Update build.gradle.kts

--- appAndroid/build.gradle.kts
+++ appAndroid/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 		compose = true
 	}
 	composeOptions {
-		kotlinCompilerExtensionVersion = "1.5.8"
+		kotlinCompilerExtensionVersion = "1.5.11"
 	}
 	kotlinOptions {
 		jvmTarget = "1.8"
