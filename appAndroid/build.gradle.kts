ddc7ead6fe6295808a167be8a7b51c2002762f1a
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-10 20:34:09 +0300

    lib update

--- appAndroid/build.gradle.kts
+++ appAndroid/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
 	implementation("com.google.android.material:material:1.11.0")
 
 	implementation("androidx.activity:activity-compose:1.8.2")
-	implementation(platform("androidx.compose:compose-bom:2022.10.00"))
+	implementation(platform("androidx.compose:compose-bom:2024.04.00"))
 
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.material:material")
