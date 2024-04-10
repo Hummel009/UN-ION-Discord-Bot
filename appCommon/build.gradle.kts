6dfb4e314928d8dd88d7ee7ca9f26d3a5c8750dd
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-10 20:48:28 +0300

    fix

--- appCommon/build.gradle.kts
+++ appCommon/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
 	implementation("net.lingala.zip4j:zip4j:2.11.5")
 	implementation("com.google.code.gson:gson:2.10.1")
-	implementation("org.apache.httpcomponents.client5:httpclient5:5.3.1.1")
+	implementation("org.apache.httpcomponents.client5:httpclient5:5.3.1")
 	implementation("org.javacord:javacord:3.8.0") {
 		exclude(group = "org.bouncycastle")
 	}
