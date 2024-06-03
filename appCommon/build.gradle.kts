88a4c6663c9a0a86c76c11c6fef34a4780c63ef0
Hummel009 <hummel.turbamentum@gmail.com>
2024-06-04 00:20:25 +0300

    d

--- appCommon/build.gradle.kts
+++ appCommon/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
 	implementation("net.lingala.zip4j:zip4j:2.11.5")
-	implementation("com.google.code.gson:gson:2.10.1")
+	implementation("com.google.code.gson:gson:2.11.0")
 	implementation("org.apache.httpcomponents.client5:httpclient5:5.3.1")
 	implementation("org.javacord:javacord:3.8.0") {
 		exclude(group = "org.bouncycastle")
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 	id("org.jetbrains.kotlin.jvm") version "2.0.0" apply false
 	id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 	id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-	id("org.jetbrains.compose") version "1.6.10" apply false
+	id("org.jetbrains.compose") version "1.6.11" apply false
 }
