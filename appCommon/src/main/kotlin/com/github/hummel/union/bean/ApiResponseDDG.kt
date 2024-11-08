fe19eeef4fbc67cf8acdc089df8e2111e2fd3bd7
Hummel009 <hummel.turbamentum@gmail.com>
2024-11-08 23:18:21 +0300

    fix

---
+++ appCommon/src/main/kotlin/com/github/hummel/union/bean/ApiResponseDDG.kt
@@ -0,0 +1,8 @@
+package com.github.hummel.union.bean
+
+data class ApiResponseDDG(
+	val action: String,
+	val status: Int,
+	val response: String,
+	val model: String
+)
--- appCommon/src/main/kotlin/com/github/hummel/union/service/impl/BotServiceImpl.kt
+++ appCommon/src/main/kotlin/com/github/hummel/union/service/impl/BotServiceImpl.kt
@@ -1,23 +1,20 @@
 package com.github.hummel.union.service.impl
 
-import com.github.hummel.union.bean.ApiResponse
+import com.github.hummel.union.bean.ApiResponseDDG
 import com.github.hummel.union.bean.ServerData
 import com.github.hummel.union.factory.ServiceFactory
 import com.github.hummel.union.lang.I18n
 import com.github.hummel.union.service.BotService
 import com.github.hummel.union.service.DataService
 import com.google.gson.Gson
-import org.apache.hc.client5.http.classic.methods.HttpPost
+import org.apache.hc.client5.http.classic.methods.HttpGet
 import org.apache.hc.client5.http.impl.classic.HttpClients
-import org.apache.hc.core5.http.ContentType
 import org.apache.hc.core5.http.io.entity.EntityUtils
-import org.apache.hc.core5.http.io.entity.StringEntity
 import org.javacord.api.entity.message.MessageBuilder
 import org.javacord.api.entity.server.Server
 import org.javacord.api.event.message.MessageCreateEvent
 import java.time.LocalDate
 import kotlin.random.Random
-import kotlin.text.replace
 
 class BotServiceImpl : BotService {
 	private val dataService: DataService = ServiceFactory.dataService
@@ -54,51 +51,32 @@ class BotServiceImpl : BotService {
 			val serverData = dataService.loadServerData(server)
 
 			if (Random.nextInt(serverData.chanceMessage) == 0) {
-				if (Random.nextInt(4) == 0) {
-					val prompt = getPrompt(event.messageContent.replace("\"", "\\\""))
-					val reply = HttpClients.createDefault().use { client ->
-						val request = HttpPost("https://api.porfirevich.com/generate/")
+				val prompt = wrapPrompt(event.messageContent)
+				val reply = HttpClients.createDefault().use { client ->
+					val request = HttpGet("https://duck.gpt-api.workers.dev/chat/?prompt=$prompt")
 
-						val payload = """
-						{
-							"prompt": "$prompt",
-							"model": "xlarge",
-							"length": 30,
-							"temperature": 0.76
-						}
-						""".trimIndent()
+					client.execute(request) { response ->
+						if (response.code in 200..299) {
+							val entity = response.entity
+							val jsonResponse = EntityUtils.toString(entity)
 
-						request.entity = StringEntity(payload, ContentType.APPLICATION_JSON)
+							val gson = Gson()
+							val apiResponse = gson.fromJson(jsonResponse, ApiResponseDDG::class.java)
 
-						request.addHeader("Accept", "*/*")
-						request.addHeader("Accept-Encoding", "gzip, deflate, br")
-						request.addHeader("Accept-Language", "ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,uk;q=0.6")
-
-						client.execute(request) { response ->
-							if (response.code in 200..299) {
-								val entity = response.entity
-								val jsonResponse = EntityUtils.toString(entity)
-
-								val gson = Gson()
-								val apiResponse = gson.fromJson(jsonResponse, ApiResponse::class.java)
-
-								cleanString(apiResponse.replies.random())
-							} else {
-								null
-							}
+							apiResponse.response
+						} else {
+							null
 						}
 					}
+				}
 
-					reply?.let {
-						MessageBuilder().apply {
-							append(reply)
-							replyTo(event.message)
-							send(event.channel)
-						}
-					} ?: run {
-						getAndSend(server, event)
+				reply?.let {
+					MessageBuilder().apply {
+						append(reply)
+						replyTo(event.message)
+						send(event.channel)
 					}
-				} else {
+				} ?: run {
 					getAndSend(server, event)
 				}
 			}
@@ -125,19 +103,8 @@ class BotServiceImpl : BotService {
 		}
 	}
 
-	private fun cleanString(input: String): String? {
-		val trimmedStart = input.trimStart { !isRussianUppercase(it) }
-
-		val trimmedEnd = trimmedStart.trimEnd { !isRussianUppercase(it) }
-
-		return if (trimmedEnd.isEmpty()) null else "$trimmedEnd!"
-	}
-
-	private fun isRussianUppercase(char: Char): Boolean =
-		char in 'А'..'Я' || char in 'Ё'..'Ё' || char in 'а'..'я' || char in 'ё'..'ё'
-
-	private fun getPrompt(input: String) =
-		"Когда Владимиру Жириновскому сказали «$input», он прокомментировал это так: "
+	private fun wrapPrompt(prompt: String): String =
+		"Один человек написал мне: «$prompt». Что мне ему ответить? Напиши ответ так, как будто это ты отвечаешь ему."
 
 	private fun getAndSend(server: Server, event: MessageCreateEvent) {
 		val crypt = dataService.getServerRandomMessage(server)
