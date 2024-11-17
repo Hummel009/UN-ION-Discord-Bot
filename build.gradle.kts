dc7929b3c636f357ab0da1b3fcc3964df1a0217f
Hummel009 <hummel.turbamentum@gmail.com>
2024-11-17 15:41:25 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	id("com.android.application") version "8.7.0-rc01" apply false
+	id("com.android.application") version "8.7.2" apply false
 	id("org.jetbrains.kotlin.jvm") version "latest.release" apply false
 	id("org.jetbrains.kotlin.android") version "latest.release" apply false
 }
