8377b3de2f30c0cd9ca35c4ad79a13849f861181
Hummel009 <hummel.turbamentum@gmail.com>
2025-02-28 19:44:35 +0300

    1

--- appCommon/src/main/kotlin/com/github/hummel/union/service/impl/UserServiceImpl.kt
+++ appCommon/src/main/kotlin/com/github/hummel/union/service/impl/UserServiceImpl.kt
@@ -127,7 +127,7 @@ class UserServiceImpl : UserService {
 				val channelId = sc.channel.get().id
 				BotData.channelHistories[channelId] = mutableListOf()
 
-				val embed = EmbedBuilder().error(sc, serverData, I18n.of("context_cleared", serverData))
+				val embed = EmbedBuilder().error(sc, serverData, I18n.of("cleared_context", serverData))
 
 				sc.createFollowupMessageBuilder().addEmbed(embed).send().get()
 			}.get()
