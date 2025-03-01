1420cc270b6f5f70a349d46a2a47d3de216024cc
Hummel009 <hummel.turbamentum@gmail.com>
2025-03-01 14:25:28 +0300

    Update LangBeBy.kt

--- appCommon/src/main/kotlin/com/github/hummel/union/lang/LangBeBy.kt
+++ appCommon/src/main/kotlin/com/github/hummel/union/lang/LangBeBy.kt
@@ -37,7 +37,7 @@ object LangBeBy : Lang {
 			"set_chance_emoji": "Усталяваны шанс эмадзі: %d.",
 			"set_chance_message": "Усталяваны шанс паведамлення: %d.",
 			"set_language": "Усталявана мова: %s.",
-			"set_preprompt": "Усталяваны прапромпт: %d",
+			"set_preprompt": "Усталяваны прапромпт: %s",
 			"set_name": "Усталявана імя: %s.",
 			
 			"current_chance_ai": "Бягучы шанс ШІ-паведамлення: %d.",
